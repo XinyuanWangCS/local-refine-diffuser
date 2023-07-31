68c5eb86bff27772f00f67096a7b400d092126db
XinyuanWangCS <xiw136@ucsd.edu>
2023-07-31 04:51:29 +0000

    small change

--- fid_sample.py
+++ fid_sample.py
@@ -35,19 +35,6 @@ from diffusers.models import AutoencoderKL
 #                             Training Helper Functions                         #
 #################################################################################
 
-@torch.no_grad()
-def update_ema(ema_model, model, decay=0.9999):
-    """
-    Step the EMA model towards the current model.
-    """
-    ema_params = OrderedDict(ema_model.named_parameters()) # Dict(name, parameter)
-    model_params = OrderedDict(model.named_parameters())
-
-    for name, param in model_params.items():
-        # TODO: Consider applying only to params that require_grad to avoid small numerical changes of pos_embed
-        ema_params[name].mul_(decay).add_(param.data, alpha=1 - decay) # add_用于权重缩减, ema_params = decay * ema_params + (1-decay) * parram
-        # param.data绕过自动微分，不会计算param的梯度 => param.detach()
-
 def requires_grad(model, flag=True):
     """
     Set requires_grad flag for all parameters in a model.
@@ -60,43 +47,6 @@ def cleanup():
     End DDP training.
     """
     dist.destroy_process_group()
-
-def create_logger(logging_dir):
-    """
-    Create a logger that writes to a log file and stdout.
-    """
-    if dist.get_rank() == 0:  # real logger: 只有排名为0的进程执行logging
-        logging.basicConfig(
-            level=logging.INFO, # 记录级别为INFO
-            format='[\033[34m%(asctime)s\033[0m] %(message)s',
-            datefmt='%Y-%m-%d %H:%M:%S',
-            handlers=[logging.StreamHandler(), logging.FileHandler(f"{logging_dir}/log.txt")]
-        )
-        logger = logging.getLogger(__name__)
-    else:  # dummy logger (does nothing)
-        logger = logging.getLogger(__name__)
-        logger.addHandler(logging.NullHandler())
-    return logger
-
-def center_crop_arr(pil_image, image_size):
-    """
-    Center cropping implementation from ADM.
-    https://github.com/openai/guided-diffusion/blob/8fb3ad9197f16bbc40620447b2742e13458d2831/guided_diffusion/image_datasets.py#L126
-    """
-    while min(*pil_image.size) >= 2 * image_size:
-        pil_image = pil_image.resize(
-            tuple(x // 2 for x in pil_image.size), resample=Image.BOX
-        )
-
-    scale = image_size / min(*pil_image.size)
-    pil_image = pil_image.resize(
-        tuple(round(x * scale) for x in pil_image.size), resample=Image.BICUBIC
-    )
-
-    arr = np.array(pil_image)
-    crop_y = (arr.shape[0] - image_size) // 2
-    crop_x = (arr.shape[1] - image_size) // 2
-    return Image.fromarray(arr[crop_y: crop_y + image_size, crop_x: crop_x + image_size])
 
 #################################################################################
 #                          Sample images for FID                                #
@@ -123,23 +73,23 @@ def generate_samples(ckpt_str, fid_dir, model, diffuser, vae, rank, device, late
     total = 0
     pbar = range(iterations)
     pbar = tqdm(pbar) if rank == 0 else pbar
-    
-    for _ in pbar:
-        z = torch.randn((n, 4, latent_size, latent_size)).to(device)
-        samples = diffuser.p_sample_loop(
-            model.forward, z.shape, z, clip_denoised = False, device = device
-        )
-        
-        samples = vae.decode(samples / 0.18215).sample
-        samples = torch.clamp(127.5 * samples + 128.0, 0, 255).permute(0, 2, 3, 1).to("cpu", dtype=torch.uint8).numpy()
-
-        for i, img in enumerate(samples):
-            index = i * dist.get_world_size() + rank + total
-            if index >= num_samples:
-                return
-            Image.fromarray(img).save(f'{ckpt_fid_samples_dir}/{index:07d}.png')
+    with torch.no_grad():
+        for _ in pbar:
+            z = torch.randn((n, 4, latent_size, latent_size)).to(device)
+            samples = diffuser.p_sample_loop(
+                model.forward, z.shape, z, clip_denoised = False, device = device
+            )
             
-        total += global_batch_size
+            samples = vae.decode(samples / 0.18215).sample
+            samples = torch.clamp(127.5 * samples + 128.0, 0, 255).permute(0, 2, 3, 1).to("cpu", dtype=torch.uint8).numpy()
+
+            for i, img in enumerate(samples):
+                index = i * dist.get_world_size() + rank + total
+                if index >= num_samples:
+                    return
+                Image.fromarray(img).save(f'{ckpt_fid_samples_dir}/{index:07d}.png')
+                
+            total += global_batch_size
 
 
 #################################################################################
@@ -191,6 +141,7 @@ def main(args):
         if rank == 0:
             print('----------------------------------------------')
             print(f'Sampling: {checkpoint}')
+        model = None
         model = DiT_Uncondition_models[args.model](input_size=latent_size)
 
         ckpt = torch.load(os.path.join(checkpoints_dir, checkpoint))
@@ -221,7 +172,7 @@ if __name__ == "__main__":
     #parser.add_argument("--data_path", type=str, required=True)
     parser.add_argument("--experiment_dir", type=str, required=True)
     parser.add_argument("--model", type=str, choices=list(DiT_Uncondition_models.keys()), default="DiT_Uncondition-S/4")
-    parser.add_argument("--image-size", type=int, choices=[128, 224, 256, 512], default=224)
+    parser.add_argument("--image-size", type=int, choices=[128, 224, 256, 512], default=256)
     parser.add_argument("--global-batch-size", type=int, default=256)
     parser.add_argument("--global-seed", type=int, default=0)
     parser.add_argument("--vae", type=str, choices=["ema", "mse"], default="ema")  # Choice doesn't affect training
