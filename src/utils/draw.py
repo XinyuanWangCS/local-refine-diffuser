0ea658fe572db6457375b7b93a21d509226415f4
XinyuanWangCS <xiw136@ucsd.edu>
2023-08-24 18:51:40 +0000

    draw

--- src/utils/draw.py
+++ src/utils/draw.py
@@ -1,32 +1,41 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import argparse
+import os
 
-# 从 Excel 文件读取数据
-data = pd.read_excel('./results/ffhq1k-013-DiT_Uncondition-S-4/eval_scores.xlsx')
 
-# 假设你的 Excel 文件中有 'x'、'y1'、'y2' 列
-x = data['ckpt_name']
-y1 = data['fid']
-y2 = data['kid']
 
-# 创建第一个图表，对应 y1 数据
-plt.figure(figsize=(8, 5))
-plt.plot(x, y1, label='y1', marker='o')
-plt.title('ckpt - fid data')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.grid(True)
-plt.legend()
-plt.savefig('line_chart_fid.png')  # 保存图表为图片文件
-plt.show()
+def main(args):
+    file_dir = os.path.join(args.experiment_dir, args.xlsx_file)
+    data = pd.read_excel(file_dir)
 
-# 创建第二个图表，对应 y2 数据
-plt.figure(figsize=(8, 5))
-plt.plot(x, y2, label='y2', marker='s')
-plt.title('ckpt - kid data')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.grid(True)
-plt.legend()
-plt.savefig('line_chart_kid.png')
-plt.show()
+    x = data['ckpt_name']
+    y1 = data['fid']
+    y2 = data['kid']
+
+    plt.figure(figsize=(8, 5))
+    plt.plot(x, y1, label='y1', marker='o')
+    plt.title('ckpt - fid data')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.grid(True)
+    plt.legend()
+    plt.savefig(os.path.join(args.experiment_dir, 'line_chart_fid.png'))  # 保存图表为图片文件
+    plt.show()
+
+    plt.figure(figsize=(8, 5))
+    plt.plot(x, y2, label='y2', marker='s')
+    plt.title('ckpt - kid data')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.grid(True)
+    plt.legend()
+    plt.savefig(os.path.join(args.experiment_dir, 'line_chart_kid.png'))
+    plt.show()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--experiment_dir", type=str, required=True)
+    parser.add_argument("--xlsx_file", type=str, default='eval_scores.xlsx')
+    args = parser.parse_args()
+    main(args)
